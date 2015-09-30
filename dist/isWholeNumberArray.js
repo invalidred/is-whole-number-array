da72b36b97cab5957f58d2d272a664ae01a45cbf
Abdul Khan <abdulkhan@gloablive.com>
2015-09-30 13:03:18 -0400

    develop: completed is-whole-number-array module

---
+++ dist/isWholeNumberArray.js
@@ -0,0 +1,33 @@
+
+/*global console*/
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var isArray = require('util').isArray;
+
+/**
+ * isWholeNumberArray is a utility function that takes an array
+ * it checks if the elements in the array are numbers.
+ * returns true if all elements are js numbers or js string  that are numbers
+ * returns false otherwise
+ */
+
+var isWholeNumberArray = function isWholeNumberArray(array) {
+  if (!isArray(array) || !array.length) {
+    return false;
+  }
+
+  var digitPattern = new RegExp("^\\d+$");
+  for (var i = 0, len = array.length; i < len; i++) {
+    if (!digitPattern.test(array[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+exports['default'] = isWholeNumberArray;
+module.exports = exports['default'];
